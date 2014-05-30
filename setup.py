bf2c26ac35ec5d795f323e41134a693dfe171fde
Souheil CHELFOUH <trollfot@gmail.com>
2014-05-30 15:54:00 +0200

    Readying release

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from os.path import join
 from setuptools import setup, find_packages
 
 name = 'uvc.design.canvas'
-version = '0.1-dev'
+version = '0.1'
 readme = open('README.txt').read()
 history = open(join('docs', 'HISTORY.txt')).read()
+
 
 install_requires = [
     'cromlech.browser',
@@ -17,9 +18,11 @@ install_requires = [
     'zope.interface',
     'zope.security',
     ]
-    
+
+
 tests_require = [
     ]
+
 
 setup(name=name,
       version=version,
