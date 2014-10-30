295bdd3c581cc48f127f231c25710630c3908aec
Souheil CHELFOUH <trollfot@gmail.com>
2014-10-30 20:05:53 +0100

    Canvas now only provides a generic baseclass 'MenuViewlet' to render menus

--- src/uvc/design/canvas/viewlets.py
+++ src/uvc/design/canvas/viewlets.py
@@ -1,73 +1,28 @@
 # -*- coding: utf-8 -*-
 
-from .managers import IAboveContent, IPageTop
-from .menus import NavigationMenu, PersonalMenu, AddMenu, ContextualActionsMenu
-from grokcore.component import baseclass, name, title, context, order
-from dolmen.viewlet import Viewlet, slot as viewletmanager
-from uvc.content.interfaces import IContent
+from grokcore.component import baseclass, context
+from dolmen.viewlet import Viewlet
 from zope.interface import Interface
 
 
-class ObjectActionMenuViewlet(Viewlet):
-    name('contextualactions')
-    title('Actions')
-    viewletmanager(IAboveContent)
-    context(IContent)
-    order(119)
-
-    def render(self):
-        menu = ContextualActionsMenu(self.context, self.request, self.view)
-        menu.update()
-        return menu.render()
-
-
-class AddMenuViewlet(Viewlet):
-    context(Interface)
-    viewletmanager(IAboveContent)
-    order(120)
-
-    def render(self):
-        menu = AddMenu(self.context, self.request, self.view)
-        menu.update()
-        return menu.render()
-
-
-class PersonalMenuViewlet(Viewlet):
-    context(Interface)
-    viewletmanager(IPageTop)
-    order(100)
-
-    def render(self):
-        menu = PersonalMenu(self.context, self.request, self.view)
-        menu.update()
-        return menu.render()
-
-
-class NavigationMenuViewlet(Viewlet):
-    context(Interface)
-    viewletmanager(IAboveContent)
-    order(100)
-
-    def render(self):
-        menu = NavigationMenu(self.context, self.request, self.view)
-        menu.update()
-        return menu.render()
-
-
-class GlobalMenuViewlet(Viewlet):
+class MenuViewlet(Viewlet):
     baseclass()
-    name('globalmenu')
     context(Interface)
-    viewletmanager(IPageTop)
-    order(11)
-
+    
     template = None
+    menu = None
+
+    def render(self):
+        if self.template is not None:
+            return super(Viewlet, self).render()
+        elif self.menu is not None:
+            menu = self.menu(self.context, self.request, self.view)
+            menu.update()
+            return menu.render()
+        else:
+            raise NotImplementedError('Please, specify a menu class')
 
 
 __all__ = [
-    'ObjectActionMenuViewlet',
-    'AddMenuViewlet',
-    'PersonalMenuViewlet',
-    'NavigationMenuViewlet',
-    'GlobalMenuViewlet',
+    'MenuViewlet',
     ]
